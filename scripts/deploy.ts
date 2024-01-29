84101cd3408310c2ddac3fd4a9b652eccbeb1b77
Siddharth Manjul <manjulsiddharth09@gmail.com>
2024-01-29 20:16:25 +0530

    Contract Deployed Successfully

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -1,22 +1,46 @@
-import { ethers } from "hardhat";
+const { ethers } = require("hardhat");
+const fs = require('fs');
+
+const deployContract = async () => {
+  let contract;
+  const taxPercent = 5;
+  const securityFeePercent = 2;
+
+  try {
+    contract = await ethers.deployContract("BookingBox", [taxPercent, securityFeePercent]);
+    await contract.waitForDeployment();
+    console.log("Contracts deployed successfully!");
+    console.log(contract.target);
+    
+    return contract;
+  } catch (error) {
+    console.log("Error deploying contract: ", error);
+    throw error;
+  }
+}
+
+const saveContractAddress = async (contract: any) => {
+  try {
+    const address = JSON.stringify({
+      BookingBoxContract: contract.target,
+    }, null, 4)
+  } catch (error) {
+    console.log("Error saving contract address: ", error);
+    throw error;
+  }
+}
 
 async function main() {
-  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  const unlockTime = currentTimestampInSeconds + 60;
+  let contract;
 
-  const lockedAmount = ethers.parseEther("0.001");
-
-  const lock = await ethers.deployContract("Lock", [unlockTime], {
-    value: lockedAmount,
-  });
-
-  await lock.waitForDeployment();
-
-  console.log(
-    `Lock with ${ethers.formatEther(
-      lockedAmount
-    )}ETH and unlock timestamp ${unlockTime} deployed to ${lock.target}`
-  );
+  try {
+    contract = await deployContract();
+    await saveContractAddress(contract);
+    console.log("Contract deployment completed successfully!");
+  } catch (error) {
+    console.log("Unhandled error: ", error);
+    
+  }
 }
 
 // We recommend this pattern to be able to use async/await everywhere
@@ -25,3 +49,6 @@ main().catch((error) => {
   console.error(error);
   process.exitCode = 1;
 });
+
+
+// Deployed address: 0x2ffA26803A967155a32203dE3a937A89B0d75dF6
